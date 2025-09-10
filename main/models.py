a7b6d1aa0d7f0446789e74c65841cc8e02e2d260
Aidam <muhammad.kaila@ui.ac.id>
2025-09-10 09:53:29 +0700

    refactor item to product

--- main/models.py
+++ main/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 from django.db import models
 
-class Item(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
