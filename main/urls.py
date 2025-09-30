137decc76696202f8feb43b943788e3f5d3da26e
aidam <aidamkaila@gmail.com>
2025-09-30 22:44:37 +0700

    tugas 5 tanpa css

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_json, show_main, add_product, show_product, show_xml, show_xml_by_id, show_json_by_id, register, login_user, logout_user
+from main.views import delete_product, edit_product, show_json, show_main, add_product, show_product, show_xml, show_xml_by_id, show_json_by_id, register, login_user, logout_user
 
 app_name = 'main'
 
@@ -14,4 +14,6 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('product/<int:id>/edit/', edit_product, name='edit_product'),
+    path('product/<int:id>/delete/', delete_product, name='delete_product'),
 ]
